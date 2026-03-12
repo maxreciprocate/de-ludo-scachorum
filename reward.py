4d460a457639e1ba02f6894d82306ac9cfcbef21
maxreciprocate <56548574+maxreciprocate@users.noreply.github.com>
2026-03-12 12:04:47 +0200

    fix(reward): set cint to original

--- reward.py
+++ reward.py
@@ -15,26 +15,39 @@ from dataclasses import asdict, dataclass
 from tokenization import decode_fen, encode_fen
 from matplotlib import pyplot as plt
 
-# stockfishpath = os.popen("whereis stockfish").read().split()[1]
-# stockfishpath = "/Users/to/pacs/stockfish/stockfish-macos-m1-apple-silicon"
 stockfishpath = "/workspace/stockfish/stockfish-ubuntu-x86-64-avx2"
 
-stockfishcfg = {"Threads": 1, "Hash": 4096}
+cgroupd = "/sys/fs/cgroup/"
+if os.path.exists(cgroupd+"cpu.max"):
+  with open(cgroupd+"cpu.max") as f:
+    parts = f.read().split()
+    cpu_count = int(parts[0]) // int(parts[1]) // 2
+elif os.path.exists(cgroupd+"cpu/cpu.cfs_quota_us"):
+  with open(cgroupd+"/cpu/cpu.cfs_quota_us") as f:
+    quota = int(f.read().strip())
+  with open(cgroupd+"/cpu/cpu.cfs_period_us") as f:
+    period = int(f.read().strip())
+  if quota > 0:
+    cpu_count = quota // period // 2
+  else:
+    cpu_count = os.cpu_count() // 2
+else:
+  cpu_count = os.cpu_count() // 2
+
+print(f'{cpu_count=}')
+
+stockfishcfg = {"Threads": 1, "Hash": 2048}
 stockfish_meganodes = int(os.environ.get("STOCKFISH_MEGANODES", 40))
-stockfish_limit = chess.engine.Limit(nodes=stockfish_meganodes * 1_000_000, time=40)
+stockfish_maxdepth = 40
+stockfish_limit = chess.engine.Limit(nodes=stockfish_meganodes * 1_000_000, time=40, depth=stockfish_maxdepth)
 
 def win_chances(score: Score) -> float:
-    """
-    winning chances from -1 to 1 https://graphsketch.com/?eqn1_color=1&eqn1_eqn=100+*+%282+%2F+%281+%2B+exp%28-0.004+*+x%29%29+-+1%29&eqn2_color=2&eqn2_eqn=&eqn3_color=3&eqn3_eqn=&eqn4_color=4&eqn4_eqn=&eqn5_color=5&eqn5_eqn=&eqn6_color=6&eqn6_eqn=&x_min=-1000&x_max=1000&y_min=-100&y_max=100&x_tick=100&y_tick=10&x_label_freq=2&y_label_freq=2&do_grid=0&do_grid=1&bold_labeled_lines=0&bold_labeled_lines=1&line_width=4&image_w=850&image_h=525
-    """
-    mate = score.mate()
-    if mate is not None:
-      return 1.0 if mate > 0 else -1.0
-
-    cp = score.score()
-    MULTIPLIER = -0.00368208 # https://github.com/lichess-org/lila/pull/11148
-
-    return 2 / (1 + math.exp(MULTIPLIER * cp)) - 1 if cp is not None else 0
+  mate = score.mate()
+  if mate is not None:
+    return 1.0 if mate > 0 else -1.0
+  cp = score.score()
+  MULTIPLIER = -0.00368208 # https://github.com/lichess-org/lila/pull/11148
+  return 2 / (1 + math.exp(MULTIPLIER * cp)) - 1 if cp is not None else 0
 
 def getboard(x):
   b = chess.Board(x["FEN"])
@@ -176,10 +189,10 @@ def evaluate(x):
 def is_realistic(board: chess.Board) -> bool:
   for color in [chess.WHITE, chess.BLACK]:
     if len(board.pieces(chess.PAWN, color)) > 8: return False
-    if len(board.pieces(chess.QUEEN, color)) > 1: return False
-    if len(board.pieces(chess.ROOK, color)) > 1: return False
-    if len(board.pieces(chess.BISHOP, color)) > 1: return False
-    if len(board.pieces(chess.KNIGHT, color)) > 1: return False
+    if len(board.pieces(chess.QUEEN, color)) > 2: return False
+    if len(board.pieces(chess.ROOK, color)) > 2: return False
+    if len(board.pieces(chess.BISHOP, color)) > 2: return False
+    if len(board.pieces(chess.KNIGHT, color)) > 2: return False
   return True
 
 def compute_score(data_source: str, solution_str: str, ground_truth: str, extra_info=None, **kwargs):
@@ -187,13 +200,10 @@ def compute_score(data_source: str, solution_str: str, ground_truth: str, extra_
   fen_distance_threshold = 6
   pv_distance_threshold = 0.3
 
-  fen = decode_fen(solution_str, "v0-verbose")
-  expanded_fen = expand_fen(fen)
-  puzzle_distance = min_fen_distance(expanded_fen)
-
-  invalid = {"score": -2, "counterint": 0, "uniqueness": 0, "penalty": 0, "valid": 0, "is_cnt": 0, "is_unq": 0, "puzzle_distance": puzzle_distance, "batch_fen_distance": None, "batch_pv_distance": None}
+  invalid = {"score": -2, "counterint": 0, "uniqueness": 0, "penalty": 0, "valid": 0, "is_cnt": 0, "is_unq": 0, "puzzle_distance": None, "batch_fen_distance": None, "batch_pv_distance": None}
 
   try:
+    fen = decode_fen(solution_str, "v0-verbose")
     board = chess.Board(fen)
     if not board.is_valid():
       print(f"invalid board: {fen}")
@@ -202,19 +212,22 @@ def compute_score(data_source: str, solution_str: str, ground_truth: str, extra_
       print(f"unreal: {fen}")
       return invalid
 
+    expanded_fen = expand_fen(fen)
+    puzzle_distance = min_fen_distance(expanded_fen)
+
     puzzle = fen_to_puzzle(fen)
+
+    pv_str = " ".join(puzzle.positions[0].eval['top']['pv'])
   except Exception as e:
     print(f"Exception in `compute_score`: {e}")
     traceback.print_exc()
     return invalid
 
-  pv_str = " ".join(puzzle.positions[0].eval['top']['pv'])
-
   is_cnt = float(puzzle.metrics['counterint'] > tau_cnt)
   is_unq = float(puzzle.uniqueness > tau_unq)
 
   # for other variants
-  score = float(is_unq and is_cnt) if not kwargs['select_score'] else kwargs['select_score'](is_unq, is_cnt)
+  score = kwargs.get('select_score')(is_unq, is_cnt) if 'select_score' in kwargs else float(is_unq and is_cnt)
 
   prior_samples = read_scored_samples()
   prior_fens = [s['expanded_fen'] for s in prior_samples]
@@ -230,7 +243,7 @@ def compute_score(data_source: str, solution_str: str, ground_truth: str, extra_
       score = 0
     else:
       append_scored_sample({"fen": fen, "expanded_fen": expanded_fen, "pv": pv_str, "score": score, "uniqueness": puzzle.uniqueness, "counterint": puzzle.metrics['counterint'], "batch_fen_distance": batch_fen_distance, "batch_pv_distance": batch_pv_distance})
-      pprint(f"cnt={puzzle.metrics['counterint']:.2f} [green]✓[/] | unq={puzzle.uniqueness:.2f} [green]✓[/] | fen_d={batch_fen_distance} | pv_d={batch_pv_distance}")
+      pprint(f"cnt={puzzle.metrics['counterint']:.2f} [green]✓[/] | unq={puzzle.uniqueness:.2f} [green]✓[/] | fen_d={f'{batch_fen_distance:.2f}' if batch_fen_distance is not None else None} | pv_d={f'{batch_pv_distance:.2f}' if batch_pv_distance is not None else None}")
 
   return {"score": score, "counterint": puzzle.metrics['counterint'], "uniqueness": puzzle.uniqueness, "penalty": puzzle.metrics['penalty'], "valid": 1, "is_cnt": is_cnt, "is_unq": is_unq, "puzzle_distance": puzzle_distance, "batch_fen_distance": batch_fen_distance, "batch_pv_distance": batch_pv_distance}
 
@@ -316,9 +329,6 @@ def fen_to_puzzle(fen: str, uniqueness_threshold=0.5) -> Puzzle:
           unq = 2.0
         else:
           unq = 1 - win_chances(scores[nmates])
-    # if eval['top'] is None:
-    #   # print("****")
-    #   unq =
     elif eval['second']:
       unq = eval['top']['winprob'] - eval['second']['winprob']
     else:
@@ -326,14 +336,17 @@ def fen_to_puzzle(fen: str, uniqueness_threshold=0.5) -> Puzzle:
 
     top_move = eval['top']['move']
     top_move_pv1_depths = [xx['depth'] for xx in eval['evaluation'] if xx['move'] == top_move and xx['multipv'] == 1]
-    cnt = min(top_move_pv1_depths, default=50) / 50
-    pnt = penalty({"FEN": b.fen()}, top_move)['penalty']
+    depth_cp = min(top_move_pv1_depths, default=1) / stockfish_maxdepth
+    pnlt = penalty({"FEN": b.fen()}, top_move)['penalty']
+    captured = b.piece_at(chess.Move.from_uci(top_move).to_square)
+    capture_material = -PIECE_VALUES.get(captured.piece_type) / 9.0 if captured else 0.0
+    cint_og = depth_cp * 0.8 + capture_material * 0.1
 
     if unq < uniqueness_threshold:
-      positions.append(Position(fen=b.fen(), top_move=top_move, eval=eval, uniqueness=unq, metrics={"counterint": cnt, "penalty": pnt}, is_unique=False))
+      positions.append(Position(fen=b.fen(), top_move=top_move, eval=eval, uniqueness=unq, metrics={"counterint": cint_og, "penalty": pnlt}, is_unique=False))
       break
 
-    positions.append(Position(fen=b.fen(), top_move=top_move, eval=eval, uniqueness=unq, metrics={"counterint": cnt, "penalty": pnt}, is_unique=True))
+    positions.append(Position(fen=b.fen(), top_move=top_move, eval=eval, uniqueness=unq, metrics={"counterint": cint_og, "penalty": pnlt}, is_unique=True))
     b.push_uci(top_move)
 
     if b.is_game_over():
@@ -350,14 +363,14 @@ def fen_to_puzzle(fen: str, uniqueness_threshold=0.5) -> Puzzle:
   unique_positions = [p for p in positions if p.is_unique]
   src = unique_positions if unique_positions else positions
   mean_uniqueness = np.mean([p.uniqueness for p in src])
-  mean_cnt = np.mean([p.metrics["counterint"] for p in src])
-  mean_pnt = np.mean([p.metrics["penalty"] for p in src])
-  return Puzzle(positions=positions, uniqueness=mean_uniqueness, metrics={"counterint": mean_cnt, "penalty": mean_pnt})
+  mean_cint = np.mean([p.metrics["counterint"] for p in src])
+  mean_pnlt = np.mean([p.metrics["penalty"] for p in src])
+  return Puzzle(positions=positions, uniqueness=mean_uniqueness, metrics={"counterint": mean_cint, "penalty": mean_pnlt})
 
 def test_puzzles():
   from datasets import load_dataset
   xs = load_dataset("Lichess/chess-puzzles", split="train[:2500]")
-  xs = xs.map(lambda x: compute_score(None, getboard(x).fen(), None), num_proc=10)
+  xs = xs.map(lambda x: compute_score(None, getboard(x).fen(), None), num_proc=cpu_count)
 
   is_unq_count = sum(xs['is_unq'])
   is_cnt_count = sum(xs['is_cnt'])
@@ -371,7 +384,6 @@ def test_puzzles():
   print(f"Both (score=1): {both_count} ({both_count/len(xs)*100:.1f}%)")
 
 def test_distance():
-  ref_fens = load_ref_fens()
   puzzles = load_dataset("Lichess/chess-puzzles", split="train[100_000:125_000]")
   distance = min_fen_distance(expand_fen(puzzles[10]['FEN']))
   print(f"fen_distance: {distance}")
@@ -394,10 +406,10 @@ def test_distance():
   print("min_pv_distance ~ all good")
 
 def test_goldenset():
-  valid = Dataset.from_json(os.path.expanduser("~/data/puzzle/goldenset-valid.jsonl"))
-  train = Dataset.from_json(os.path.expanduser("~/data/puzzle/goldenset-train.jsonl"))
-  valid = valid.map(lambda x: asdict(fen_to_puzzle(x["FEN"])), num_proc=os.cpu_count() // 2)
-  train = train.map(lambda x: asdict(fen_to_puzzle(x["FEN"])), num_proc=os.cpu_count() // 2)
+  valid = Dataset.from_json(os.path.expanduser("../../data/opus/goldenset-valid.jsonl"))
+  train = Dataset.from_json(os.path.expanduser("../../data/opus/goldenset-train.jsonl"))
+  valid = valid.map(lambda x: asdict(fen_to_puzzle(x["FEN"])), num_proc=cpu_count)
+  train = train.map(lambda x: asdict(fen_to_puzzle(x["FEN"])), num_proc=cpu_count)
 
   allset = concatenate_datasets([train, valid])
   apvalid = average_precision([m['counterint'] for m in valid['metrics']], valid['label'])
@@ -426,5 +438,5 @@ def test_goldenset():
   plt.show()
 
 if __name__ == '__main__':
-  # test_goldenset()
-  test_distance()
+  test_goldenset()
+  # test_distance()
